4fcef859ac413dc93ea9cc831992f89ca7a7b985
Pranjal Patel <patelpranjal139@gmail.com>
2023-09-13 23:45:50 +0530

    Updated Python files

--- weather_notify.py
+++ weather_notify.py
@@ -6,25 +6,6 @@ n = ToastNotifier()
 url = "https://freemeteo.in/weather/Ahmedabad/current-weather/location/?gid=9034993&language=english&country=india"
 r = requests.get(url, headers={'User-agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'})
 
+api = "dfda56122d04af1b23790dd7d2ffc801"
 
-print(r.html.find('title', first=True).text)
-
-
-# soup = BeautifulSoup(htmldata, 'html.parser')
-# title = soup.title
-# # print(type(title))
-# # print(type(soup.navigation))
-# # print(type(soup.string))
-
-# current_temp = soup.find_all("div", class_="temp")
-
-# wind_speed= soup.find_all("span", class_="white")
-# # print(soup.find_all("span", class_="white")) \
-
-
-# temp = (str(current_temp))
-
-# wind = (str(wind_speed))
-
-# result = (f"Current Temp:{temp} Wind Speed:{wind}")
-# print(result)
+# https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
